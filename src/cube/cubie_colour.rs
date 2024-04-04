e9436f68110b1f98838f5be52160c9df18fab3f0
Mike Croall <mikebcroall@gmail.com>
2024-04-05 00:22:58 +0100

    Test cubie_colour.rs

--- src/cube/cubie_colour.rs
+++ src/cube/cubie_colour.rs
@@ -44,3 +44,80 @@ impl CubieColour {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use colored::Color;
+    use paste::paste;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_default_char_cubie() {
+        let cubie = CubieColour::Red(None);
+        let displayed_char = cubie
+            .get_coloured_display_char()
+            .normal()
+            .chars()
+            .next()
+            .unwrap();
+
+        assert_eq!(DEFAULT_CUBIE_CHAR, displayed_char);
+    }
+
+    #[test]
+    fn test_custom_char_cubie() {
+        let cubie = CubieColour::Red(Some('?'));
+        let displayed_char = cubie
+            .get_coloured_display_char()
+            .normal()
+            .chars()
+            .next()
+            .unwrap();
+
+        assert_eq!('?', displayed_char);
+    }
+
+    macro_rules! colour_tests {
+        ($($cubie_constructor:ident, $rgb:expr,)*) => {
+            paste! {
+                colour_tests!(
+                    $(
+                        [<test_ $cubie_constructor:lower _cubie>] , $cubie_constructor, $rgb,
+                    )*
+                );
+            }
+        };
+        ($($test_name:ident, $cubie_constructor:ident, $rgb:expr,)*) => {
+            $(
+                #[test]
+                fn $test_name() {
+                    let cubie = CubieColour::$cubie_constructor(Some('?'));
+                    let display_char = cubie.get_coloured_display_char();
+                    let colour_opt = display_char.fgcolor();
+                    assert!(colour_opt.is_some());
+                    let colour = colour_opt.unwrap();
+
+                    let (r, g, b) = $rgb;
+                    let expected_colour = Color::TrueColor { r, g, b };
+                    assert_eq!(expected_colour, colour);
+                }
+            )*
+        };
+    }
+
+    colour_tests!(
+        Blue,
+        (0, 0, 255),
+        Green,
+        (0, 255, 0),
+        Orange,
+        (255, 127, 0),
+        Red,
+        (255, 0, 0),
+        White,
+        (255, 255, 255),
+        Yellow,
+        (255, 255, 0),
+    );
+}
