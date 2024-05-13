1579f88494f90e3a15615d380a79ab701133a0c7
Mike Croall <mikebcroall@gmail.com>
2024-05-13 22:55:52 +0100

    gui: test some of defaults.rs

--- puzzle-cube-ui/src/gui/defaults.rs
+++ puzzle-cube-ui/src/gui/defaults.rs
@@ -24,3 +24,32 @@ pub(super) fn initial_camera(viewport: Viewport) -> Camera {
 pub(super) fn clear_state() -> ClearState {
     ClearState::color_and_depth(0.13, 0.13, 0.13, 1.0, 1.0)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use three_d::Vector3;
+
+    #[test]
+    fn test_initial_camera_targets_origin() {
+        let camera = initial_camera(Viewport {
+            x: 0,
+            y: 0,
+            width: 75,
+            height: 50,
+        });
+
+        assert_eq!(camera.target(), &Vector3::new(0., 0., 0.));
+    }
+
+    #[test]
+    fn test_clear_state_is_monochrome() {
+        let clear_state = clear_state();
+
+        assert_eq!(clear_state.red, clear_state.green);
+        assert_eq!(clear_state.red, clear_state.blue);
+        assert_eq!(clear_state.alpha, Some(1.));
+        assert_eq!(clear_state.depth, Some(1.));
+    }
+}
